223e72bb040b6c706577710a9d053fe438cc7e13
Pedro Anderson de Souza Bispo <pasbispo@gmail.com>
2025-05-06 12:40:42 -0300

    Corrigindo a exibição de imagens estáticas no GitHub Pages

--- static/menu.js
+++ static/menu.js
@@ -1,18 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
     let menuButton = document.getElementById("menu-more");
-    
+
     if (menuButton) {
         menuButton.addEventListener("click", function (event) {
             event.preventDefault();
-            let menu = this.nextElementSibling; // Encontra o menu correto dentro da <li>
+            let menu = this.parentElement.querySelector(".menu-container"); // Seleciona apenas o menu correto
 
             if (menu.style.display === "block") {
                 menu.style.opacity = "0";
+                menu.style.visibility = "hidden";
                 setTimeout(() => {
                     menu.style.display = "none";
                 }, 300);
             } else {
                 menu.style.display = "block";
+                menu.style.visibility = "visible";
                 setTimeout(() => {
                     menu.style.opacity = "1";
                 }, 10);
