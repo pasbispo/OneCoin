4f57697bb060f9aa4fa86e509afc6d9320e3ad67
Pedro Anderson de Souza Bispo <pasbispo@gmail.com>
2025-05-06 10:59:18 -0300

    Corrigindo a exibição de imagens estáticas no GitHub Pages

--- static/menu.js
+++ static/menu.js
@@ -1,19 +1,25 @@
+  document.addEventListener("DOMContentLoaded", function () {
+    let menuButton = document.getElementById("menu-more");
+    
+    if (menuButton) {
+        menuButton.addEventListener("click", function (event) {
+            event.preventDefault();
+            let menu = this.nextElementSibling; // Seleciona o menu correto dentro da <li>
 
-
-   document.getElementById("menu-more").addEventListener("click", function (event) {
-    event.preventDefault();
-    let menu = this.nextElementSibling;
-
-    if (menu.style.display === "block") {
-        menu.style.opacity = "0";
-        setTimeout(() => {
-            menu.style.display = "none";
-        }, 300);
+            if (menu.style.display === "block") {
+                menu.style.opacity = "0";
+                setTimeout(() => {
+                    menu.style.display = "none";
+                }, 300);
+            } else {
+                menu.style.display = "block";
+                setTimeout(() => {
+                    menu.style.opacity = "1";
+                }, 10);
+            }
+        });
     } else {
-        menu.style.display = "block";
-        setTimeout(() => {
-            menu.style.opacity = "1";
-        }, 10);
+        console.error("Botão 'Mais' não encontrado!");
     }
 });
 
