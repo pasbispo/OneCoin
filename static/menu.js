ed3ec2dfc4c3a67300a81dcc0baf0509c2aaf025
Pedro Anderson de Souza Bispo <pasbispo@gmail.com>
2025-05-06 01:17:30 -0300

    Corrigindo a exibição de imagens estáticas no GitHub Pages

--- static/menu.js
+++ static/menu.js
@@ -1,16 +1,12 @@
 document.getElementById("menu-more").addEventListener("click", function (event) {
     event.preventDefault();
-    let menu = this.parentElement.querySelector(".menu-container"); // Seleciona apenas o menu correto
+    let menu = this.nextElementSibling;
 
-    if (menu.style.display === "block") {
+    if (menu.style.visibility === "visible") {
         menu.style.opacity = "0";
-        setTimeout(() => {
-            menu.style.display = "none";
-        }, 300);
+        menu.style.visibility = "hidden";
     } else {
-        menu.style.display = "block";
-        setTimeout(() => {
-            menu.style.opacity = "1";
-        }, 10);
+        menu.style.visibility = "visible";
+        menu.style.opacity = "1";
     }
 });
