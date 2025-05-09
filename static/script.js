21aafabc4dc62191128139a919e97a659363b88f
Pedro Anderson de Souza Bispo <pasbispo@gmail.com>
2025-05-08 23:56:01 -0300

    Corrigindo a exibição de imagens estáticas no GitHub Pages

--- static/script.js
+++ static/script.js
@@ -2,3 +2,10 @@ document.getElementById("site-logo").addEventListener("click", function(event) {
     event.preventDefault(); // Evita comportamento padrão do link
     window.location.href = "index.html"; // Redireciona para a página principal
 });
+
+
+
+document.getElementById("signup-button").addEventListener("click", function(event) {
+    event.preventDefault(); // Impede o comportamento padrão do link
+    window.location.href = "cadastro.html"; // Redireciona para a página de cadastro
+});
