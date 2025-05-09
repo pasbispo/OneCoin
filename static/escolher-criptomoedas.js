0589e44f3c34610b9a3e10e0b53f2a1eb6c07642
Pedro Anderson de Souza Bispo <pasbispo@gmail.com>
2025-05-09 15:52:40 -0300

    Corrigindo a exibição de imagens estáticas no GitHub Pages

--- static/escolher-criptomoedas.js
+++ static/escolher-criptomoedas.js
@@ -58,3 +58,14 @@ function limitDecimals(input) {
 
     input.value = value;
 }
+
+
+function selectCrypto(crypto, name) {
+    let cryptoImage = document.getElementById("crypto-image");
+    let cryptoName = document.getElementById("crypto-name");
+
+    cryptoImage.src = `static/img/${crypto}.png`;
+    cryptoImage.classList.remove("hidden"); // Exibir imagem
+    cryptoName.textContent = name; // Atualizar nome da criptomoeda
+}
+
