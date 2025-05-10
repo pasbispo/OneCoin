a479c52f142eb60906dcfc2cc38b6826098536ff
Pedro Anderson de Souza Bispo <pasbispo@gmail.com>
2025-05-10 20:19:01 -0300

    Corrigindo a exibição de imagens estáticas no GitHub Pages

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const cors = require('cors'); // Para permitir requisições do seu frontend
 const app = express();
 app.use(cors());
 
-app.get('/crypto', async (req, res) => {
-    const url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC&convert=USD';
+app.get('/crypto/:symbol', async (req, res) => {
+    const crypto = req.params.symbol.toUpperCase();
+    const url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=${crypto}&convert=USD`;
     const options = { headers: { 'X-CMC_PRO_API_KEY': 'SUA_CHAVE_API' } };
 
     try {
@@ -17,5 +18,6 @@ app.get('/crypto', async (req, res) => {
         res.status(500).json({ error: 'Erro ao buscar dados' });
     }
 });
+
 
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
